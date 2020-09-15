c034b64012cb030f8dc904650c4114aea9fd652d
Mike Siddall <eatingartist@gmail.com>
2020-09-15 10:19:51 -0500

    render PictureGallery

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//If this were node....
+//const PictureGallery = require('./PictureGallery')
+
+// But on client side we do..
+import PictureGallery from './PictureGallery';
+
+//React Component Incantation
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Happy React Day!</h1>
+
+        <h3>Things to lear about React:</h3>
+        <ul>
+          <li>HTML inside of JS files</li>
+          <li>class App extends Component?!</li>
+          <li>render()?</li>
+          <li>import / export</li>
+
+
+        </ul>
+        <h3>Check out my picture gallery:</h3>
+        <PictureGallery />
       </div>
     );
   }
---
+++ src/PictureGallery.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class PictureGallery extends Component {
+
+    render() {
+       return (
+           <div>
+               <img src="https://picsum.photos/200/200"></img>
+               <img src="https://picsum.photos/200/200"></img>
+               <img src="https://picsum.photos/200/200"></img>
+               <img src="https://picsum.photos/200/200"></img>
+               <img src="https://picsum.photos/200/200"></img>
+               <img src="https://picsum.photos/200/200"></img>
+           </div>
+       ); 
+    }
+
+}    
+// if this were node...
+
+//module.exports = PictureGallery;
+
+// But on the client-side we do:
+
+export default PictureGallery;
